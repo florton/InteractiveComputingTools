050feb21d03cb048ea105e6857b1218162ce86ef
Flanders Lorton <florton@lion.lmu.edu>
2017-01-03 02:22:41 -0700

    Added lights

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import sys, pygame, math
 
-# A gate is a list [image, rect, gate_name_string, connections_on_inputs] 
+# A gate is a list [image, rect, gate_name_string, connections_on_input_anchors] 
 def makeGate(name):
     gate = pygame.image.load("gatePics\\" + name + ".png")
     gateRect = gate.get_rect()
@@ -13,8 +13,15 @@ def makeSwitch():
     switchRect = switch.get_rect()
     switchRect.center = mouseX,mouseY 
     return([switch,switchRect,False,[]])
+
+# A light is a list [image, rect, on/off, connections] 
+def makeLight():
+    light = pygame.image.load("gatePics\LIGHTOFF.png")
+    lightRect = light.get_rect()
+    lightRect.center = mouseX,mouseY 
+    return([light,lightRect,0,[]])    
     
-#A line is a matrix list [[start_target_edge, start_target , start_coords],[end_target_edge, end_target, end_coords]]    
+#A line is a matrix list [[start_target_anchor, start_target , start_coords],[end_target_anchor, end_target, end_coords]]    
 def makeLine(mouseX,mouseY,target,drawingLine):
     mouseRelativeX = mouseX-target[1].left
     mouseRelativeY = mouseY-target[1].top
@@ -25,6 +32,9 @@ def makeLine(mouseX,mouseY,target,drawingLine):
     #check if switch
     if type(target[2]) == type(True):
         anchor = 4
+    #else check if light
+    elif type(target[2]) == type(1):
+        anchor = 5
     #otherwise its a gate    
     elif mouseRelativeX > 2*(target[1].width/3):
         anchor = 1
@@ -37,13 +47,15 @@ def makeLine(mouseX,mouseY,target,drawingLine):
             anchor = 3
     if drawingLine == 1:        
         loadedLines.append([[anchor,target,(mouseX,mouseY)],[None,None,None]])
-    elif anchor != loadedLines[-1][0][0]:    
+    elif (anchor in [5,0,2,3] and loadedLines[-1][0][0] in [1,4]) or  (anchor in [1,4] and loadedLines[-1][0][0] in [5,0,2,3]):       
         loadedLines[-1][1] = [anchor,target,(mouseX,mouseY)]
         pygame.mouse.set_cursor(*pygame.cursors.arrow)
         if anchor == 1 or anchor == 4:
             loadedLines[-1][0][1][3] = target
+            print loadedLines[-1][0][1]
         else:
             target[3].append(loadedLines[-1][0][1])
+            print target
         return 3
     return 2
     
@@ -64,7 +76,9 @@ def IsBetween(startPoint,endPoint,newPoint):
         
 def DeleteLine(line):
     target = line[1][1]
-    target[3].remove(line[0])
+    print target
+    if target:
+        target[3].remove(line[0])
     loadedLines.remove(line)
     
 def Click(clickCoords):
@@ -80,24 +94,35 @@ def Click(clickCoords):
             
 def UpdateLines():
     for line in loadedLines:
-        newCoords = [None,(mouseX,mouseY)]
-        offset = gate[1].width/10
+        newCoords = [None,(mouseX,mouseY)]        
         for x in range(2):
             target = line[x][1]
+            offset = target[1].width/10 if target else 0
+            #if NOT gate input
             if line[x][0] == 0:
                 newCoords[x] = target[1].midleft[0]+offset, target[1].midleft[1]
+            #if gate output
             elif line[x][0] == 1:
                 newCoords[x] = target[1].midright[0]-offset, target[1].midright[1]
+            #if gate input bottom
             elif line[x][0] == 2:
                 newCoords[x] = target[1].left+offset , target[1].topleft[1] + 3*(target[1].height/4)
+            #if gate input top
             elif line[x][0] == 3:
                 newCoords[x] = target[1].left+offset , target[1].topleft[1] + target[1].height/4
+            #if switch
             elif line[x][0] == 4:
                 newCoords[x] = target[1].midright
+            #if light
+            elif line[x][0] == 5:
+                newCoords[x] = target[1].midleft
         line[0][2] = newCoords[0]
         line[1][2] = newCoords[1]
         pygame.draw.line(screen, red, newCoords[0], newCoords[1], 2)
+
+##Main()
         
+#Initialize variables 
 pygame.init()
 
 size = width, height = 800, 600
@@ -118,6 +143,7 @@ drawingLine = False
 loadedGates = []
 loadedLines = []
 loadedSwitches = []
+loadedLights = []
 
 gateSelect = pygame.image.load("gatePics\GATES.png")
 selectRect = gateSelect.get_rect()
@@ -130,6 +156,10 @@ lineButtonRect.midleft = (0, height/4)
 switchButton = pygame.image.load("gatePics\SWITCH.png")
 switchButtonRect = switchButton.get_rect()
 switchButtonRect.midleft = (0, 3*(height/8))
+
+lightButton = pygame.image.load("gatePics\LIGHTBUTTON.png")
+lightButtonRect = lightButton.get_rect()
+lightButtonRect.midleft = (0, height/2)
 
 #Main Loop
 while True:
@@ -158,17 +188,13 @@ while True:
     if draggingObject and not drawingLine:
         draggingObject[1].center = mouseX, mouseY
     elif mouseKey[0] == 1:
-        for target in loadedGates+loadedSwitches:
+        for target in loadedGates+loadedSwitches+loadedLights:
             if target[1].collidepoint(mouseX,mouseY):
                 #maybe draw a line instead
-                if drawingLine:
+                if drawingLine and drawingLine != 3:
                     drawingLine = makeLine(mouseX,mouseY,target,drawingLine)                   
-                else:
+                elif drawingLine != 3:
                     draggingObject = target
-        #move and flip switches
-        for switch in loadedSwitches:
-            if switch[1].collidepoint(mouseX,mouseY):
-                draggingObject = switch
     if mouseKey[0] == 0 and draggingObject:
         if draggingObject and selectRect.collidepoint(draggingObject[1].center):
             Delete(draggingObject)
@@ -200,6 +226,9 @@ while True:
         #Spawn switches
         if switchButtonRect.collidepoint(mouseX,mouseY):
             loadedSwitches.append(makeSwitch())
+        #Spawn lights
+        if lightButtonRect.collidepoint(mouseX,mouseY):
+            loadedLights.append(makeLight())
             
     #Start Drawing
     screen.fill(white) 
@@ -207,12 +236,16 @@ while True:
     screen.blit(gateSelect,selectRect)   
     screen.blit(lineButton,lineButtonRect)  
     screen.blit(switchButton,switchButtonRect)     
-    #draw all gates
-    for gate in loadedGates:
-        screen.blit(gate[0], gate[1])  
+    screen.blit(lightButton,lightButtonRect)     
+    #draw all gates, switches & Lines
+    for target in loadedGates+loadedSwitches+loadedLights:
+        screen.blit(target[0], target[1])  
     #draw all switches
     for switch in loadedSwitches:
         screen.blit(switch[0],switch[1])
+    #draw all lights
+    for light in loadedLights:
+        screen.blit(light[0],light[1])
     #draw and update all lines
     UpdateLines()
     #draw text
